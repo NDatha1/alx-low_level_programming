7a45921931d7dd53ddb6824beb4f530884f2f00e
Kennedy Ndatha <kennedyndatha1@gmail.com>
2023-08-10 02:54:53 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - a c program that print s given content followed by a new line.
+ * Return: 0 {Success}
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle\n");
+	return (0);
+}
