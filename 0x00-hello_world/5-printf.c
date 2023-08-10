4de82f0a5505f3caeef2ee51e0fd40b93a6e763c
Kennedy Ndatha <kennedyndatha1@gmail.com>
2023-08-10 03:26:44 -0700

    5. Hello, printf update1

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 /**
- * main - A line that prints using printf
+ * main - A line that prints using printf Function
  * return: 0(Success)
- * */
+ */
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
