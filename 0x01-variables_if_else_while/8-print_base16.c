452093c739aef83c15661535116f523c750fb6a3
Kennedy Ndatha <kennedyndatha1@gmail.com>
2023-08-11 07:32:57 -0700

    8. Hexadecimal

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - Entry point of code
+ * Return: 0 (Sucees)
+ */
+int main(void)
+{
+	int m;
+	char n;
+
+	for(m = '0'; m <= '9'; m++)
+	putchar(m);
+
+	for (n = 'a'; n <= 'f'; n++)
+	putchar(n);
+	putchar('\n');
+
+	return (0);
+}
