13d4b42640c54260439208db6f1e1d8747c5dbaa
Kennedy Ndatha <kennedyndatha1@gmail.com>
2023-08-11 06:44:09 -0700

    5. Numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	unsigned int a;
+
+	for (a = 0; a <= 9; a++)
+	printf("%d", a);
+	printf("\n");
+
+	return (0);
+}	
