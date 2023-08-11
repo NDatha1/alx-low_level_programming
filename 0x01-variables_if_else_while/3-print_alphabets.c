94a5f9d267bd25dcdd8ccddc5c12311b07c01f9a
Kennedy Ndatha <kennedyndatha1@gmail.com>
2023-08-11 06:01:39 -0700

    3. alphABET

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+ * main - Entry point of code
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	char n;
+	char m;
+
+	n = 'a';
+	m = 'A';
+	while (n <= 'z')
+	{
+		putchar(n);
+		n++;
+	}
+	while (m <= 'Z')
+	{
+		putchar(m);
+		m++;
+	}
+	putchar('\n');
+	return (0);
+}
