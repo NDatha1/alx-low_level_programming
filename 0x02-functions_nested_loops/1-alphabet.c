48a7750651b3d988404e2c89e801acc0f9f77aea
Kennedy Ndatha <kennedyndatha1@gmail.com>
2023-08-15 01:23:28 -0700

    1-alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * print_alphabet - Make the lowercase alphabet
+ * Return: 0 (Success)
+ */
+void print_alphabet(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+	_putchar(c);
+	}
+	_putchar('\n');
+}
