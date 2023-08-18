aacdd7ba38a1783d541e16cbbaf3d7529a3f480e
Kennedy Ndatha <kennedyndatha1@gmail.com>
2023-08-18 07:03:03 -0700

    2. Collaboration is multiplication

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * mul - Multiply 2 numbers
+ * @a: Integer to be multiplied
+ * @b: Integer to be multiplied
+ * Return: Always 0.
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
