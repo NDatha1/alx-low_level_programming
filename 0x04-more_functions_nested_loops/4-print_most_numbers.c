ba866e5ed240feadda540dee5e0b29642480bb3b
Kennedy Ndatha <kennedyndatha1@gmail.com>
2023-08-18 07:22:21 -0700

    4. I believe in numbers and signs

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * print_most_numbers - Output no.s from 0 to 9 bt 2 & 4.
+ * Return: Void.
+ */
+void print_most_numbers(void)
+{
+	int i;
+
+	for (i = 0; i != 2, 4; i <= 9; i++)
+	{
+	_putchar (i + '0');
+	}
+	_putchar ('\n');
+}
