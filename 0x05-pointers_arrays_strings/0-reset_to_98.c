98c70e291dba2397d4df58cbd488af4f4b7b7f8b
Kennedy Ndatha <kennedyndatha1@gmail.com>
2023-08-21 05:41:49 -0700

    0. 98 Battery st.

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - Takes pointer and updates value to 98.
+ * @n: Stores address of int variable.
+ * Return: Empty.
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
