ea83ca054170fc96a178eb4c9ef8380d73ac4c0e
Kennedy Ndatha <kennedyndatha1@gmail.com>
2023-09-11 03:18:55 -0700

    0. Poppy

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - Info on dog.
+ * @name:Identifier of dog.
+ * @age: No. of years.
+ * @owner: The master.
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
